0fdf1e5dfe21dfa75e07f446f01cd7f3982f2e56
jacksmedia <alex@j4cks.com>
2022-01-29 16:28:39 -0800

    :wrench: typographic errata

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,11 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/mainshib.png`, // This path is relative to the root of the site.
       },
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/components/typography`,
+        omitGoogleFont: true
+      },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
