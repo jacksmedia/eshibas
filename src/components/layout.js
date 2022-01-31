c1a9d675fcca302f3057ae1cc2c82fd26dd719f9
jacksmedia <alex@j4cks.com>
2022-01-30 19:42:00 -0800

    :art: incremental styling tools built

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,9 +35,17 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer className="footer-styling">
-          © {new Date().getFullYear()}, 
-          {` `}
-          <a href="https://www.jacks.media">Jacks Media</a>
+          <p className="white-txt">
+            MADE WITH LOVE BY ELROND SHIBAS
+          </p>
+          <p className="white-txt">
+          Cdo: ERD1X5XYD7WLL5US3RLZTZYFSC8SAZPH5DNRQEN8V90PR34GFPR7S5VQDJV8D7
+          </p>
+          <p className="white-txt">
+            © {new Date().getFullYear()}, 
+            {` `}
+            <a href="https://www.jacks.media">Jacks Media</a>
+          </p>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,19 +12,31 @@ const IndexPage = () => (
     <p>Welcome to the NFT era, fren!</p>
     <p>Elrond blockchain is home to 5000 Shiba pets that pay you!</p>
     <h1>Join us in Shibaworld.</h1>
-    <StaticImage
-      src="../images/es164.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="An Elrond Shiba this one is number 164"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
+    <div className="level">
+      <StaticImage
+        className="tile"
+        src="../images/es164.png"
+        width={275}
+        height={275}
+        quality={95}
+        formats={["auto", "webp", "avif"]}
+        alt="An Elrond Shiba this one is number 164"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p className="tile">You hear a low rumbling thunder in the distance followed by crackling and an explosion! What's happening? 
+        That's the sound of Elrond Shibas headed to the moon! 
+        Cute collectible pixel Shibas and a thriving community!
+        Join the pack today! #GMI
+      </p>
+    </div>
+    <div className="level is-mobile wafl-background act-zone">
+      <h1 className="syrup-text">WAFL</h1>
+    </div>
+    <div className="level is-mobile">
       <Link to="/rarity/">Rarity</Link>
       <Link to="/social">Social Links</Link>
       <Link to="/wave-6">Wave 6</Link>
-    </p>
+    </div>
   </Layout>
 )
 
