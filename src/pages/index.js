009f4bed8820a72c709d6a88a0d1c498d7909552
jacksmedia <alex@j4cks.com>
2022-02-06 17:05:48 -0800

    :construction: more sections landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,15 +13,104 @@ const IndexPage = () => (
       <p className="">Welcome to the NFT era, fren!</p>
       <p>Elrond blockchain is home to 5000 Shiba pets that pay you!</p>
       <h1>Join us in Shibaworld.</h1>
+      <div className="columns">
+        <div className="column gray">1</div>
+        <div className="column gray">2</div>
+        <div className="column gray">3</div>
+        <div className="column gray">4</div>
+      </div>
     </div>
+
     <div className="section spacer-zone">
-      <p>#wagmi</p>
+       <div className="columns">
+        <div className="column gray">
+          <h2>#wagmi</h2>
+          <h2>Join Us</h2>
+          <p>Welcome to the NFT era, fren! Elrond blockchain is home to 5000 Shiba pets that pay you!</p>
+          <button className="shibutton">DISCORD</button>
+        </div>
+        <div className="column gray">big slider of 3</div>
+      </div>
     </div>
     <div className="section spacer-zone eshib-bg-1">
       <h1 className="dark-forest-text">From the deep forests of Egold, here come the Elrond Shibas!</h1>
       <div className="slideshow level is-mobile">
         <div className="images01"></div>
-        <p className="">The entire collection comprises 5000 NTFs and 7777 SFTs.</p>
+      </div>
+    </div>
+
+    <div className="section spacer-zone">
+       <div className="columns">
+        <div className="column gray">
+          <h2>#wagmi</h2>
+          <h2>Join Us</h2>
+          <p>Welcome to the NFT era, fren! Elrond blockchain is home to 5000 Shiba pets that pay you!</p>
+          <button className="shibutton">DISCORD</button>
+        </div>
+        <div className="column gray">big slider of 3</div>
+      </div>
+    </div>
+    <div className="section spacer-zone eshib-bg-3">
+      <h1 className="dark-forest-text">From the deep forests of Egold, here come the Elrond Shibas!</h1>
+      <div className="slideshow level is-mobile">
+        <div className="images01"></div>
+      </div>
+    </div>
+
+
+    <div className="section spacer-zone">
+       <div className="columns">
+        <div className="column gray">
+          <h2>#wagmi</h2>
+          <h2>Join Us</h2>
+          <p>Welcome to the NFT era, fren! Elrond blockchain is home to 5000 Shiba pets that pay you!</p>
+          <button className="shibutton">DISCORD</button>
+        </div>
+        <div className="column gray">big slider of 3</div>
+      </div>
+    </div>
+    <div className="section spacer-zone eshib-bg-21">
+      <h1 className="dark-forest-text">From the deep forests of Egold, here come the Elrond Shibas!</h1>
+      <div className="slideshow level is-mobile">
+        <div className="images01"></div>
+      </div>
+    </div>
+
+
+    <div className="section spacer-zone">
+       <div className="columns">
+        <div className="column gray">
+          <h2>#wagmi</h2>
+          <h2>Join Us</h2>
+          <p>Welcome to the NFT era, fren! Elrond blockchain is home to 5000 Shiba pets that pay you!</p>
+          <button className="shibutton">DISCORD</button>
+        </div>
+        <div className="column gray">big slider of 3</div>
+      </div>
+    </div>
+    <div className="section spacer-zone eshib-bg-22">
+      <h1 className="dark-forest-text">From the deep forests of Egold, here come the Elrond Shibas!</h1>
+      <div className="slideshow level is-mobile">
+        <div className="images01"></div>
+      </div>
+    </div>
+
+
+    <div className="section spacer-zone">
+       <div className="columns">
+        <div className="column gray">
+          <h2>#wagmi</h2>
+          <h2>Join Us</h2>
+          <p>Welcome to the NFT era, fren! Elrond blockchain is home to 5000 Shiba pets that pay you!</p>
+          <button className="shibutton">DISCORD</button>
+        </div>
+        <div className="column gray">big slider of 3</div>
+      </div>
+    </div>
+    <div className="section spacer-zone eshib-bg-5">
+      <h1 className="dark-forest-text">From the deep forests of Egold, here come the Elrond Shibas!</h1>
+      <div className="slideshow level is-mobile">
+        <div className="images01"></div>
       </div>
     </div>
 
