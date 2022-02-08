406f93f5321dd93c86d266f89a94bb7c320b9cf8
jacksmedia <alex@j4cks.com>
2022-02-08 15:36:29 -0800

    :waxing_gibbous_moon: tuning responsive layout, rm bad content

--- src/pages/roadmap.js
+++ src/pages/roadmap.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 import Seo from "../components/seo"
 
 const Roadmap = () => (
@@ -20,18 +21,18 @@ const Roadmap = () => (
       </div>
       <div className="container pt-3 pl-3 is-fluid">
         <h2>Historical Progress</h2>
-        <p>December 2021 - First wave</p>
-        <p>January 2022 - Second, Third, Fourth Waves</p>
-        <p>February 2022 - Fifth Wave Sells out in 3 minutes</p>
+        <p>December 2021 - First wave (0.25 EGLD)</p>
+        <p>January 2022 - Second, Third, Fourth Waves (0.3 - 0.5 EGLD)</p>
+        <p>February 2022 - Fifth Wave Sells out in <strong>3 minutes</strong> (0.6 EGLD)</p>
+        <p>February 2022 - Wave 6 / 0.8 EGLD / 300 Shibas</p>
       </div>
       <div className="container pt-3 pl-3 is-fluid">
-      <h2>Upcoming Waves</h2>
+      <h2>Upcoming</h2>
         <ul>
-          <li><strong>Current Wave 6</strong>, 0.8 EGLD, 300 Shibas</li>
           <li>50 EGLD Sweepstakes - for all Discord Members - No Purchase Necessary (20 Winners)</li>
           <li><strong>Wave 7</strong>, ??? EGLD. ??? Shibas</li>
           <li>75 EGLD weepstakes - for all Discord Members - No Purchase Necessary (20 Winners)</li>
-          <li><strong>Wave 8</strong>,  ??? EGLD, remaining of 5000 Shibas</li> 
+          <li><strong>Wave 8 & Beyond</strong>,  ??? EGLD, remaining 5000 Shibas</li> 
           <li>100 EGLD Sweepstakes - for all Discord Members - No Purchase Necessary (20 Winners)</li>
         </ul>  
       </div>
@@ -40,6 +41,16 @@ const Roadmap = () => (
 
     <div className="container pt-3 pl-3 is-fluid">
       <h2> Anime Shibas (ANISHIB) and 3D Shibas (3DSHIB) NFT collections</h2>
+      <StaticImage
+            className=""
+            layout="fixed"
+            formats={["AUTO", "GIF", "AVIF"]}
+            src="../images/uwu.gif"
+            width={185}
+            height={185}
+            quality={95}
+            alt="Elrond AniShibs loves you"
+          />
       <ul>
         <li>5000 Supply, each collections</li>
         <li>2,500 Buyable ONLY with $WAFL</li>
